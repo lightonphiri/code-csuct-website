79d091991927be1c12bb97eeb1368682e7468f04
Lighton Phiri <lighton.phiri@gmail.com>
2018-10-29 10:00:32 +0200

    Update on permanently leaving The UCT
    Changed Weg page profile theme
    Added textual content indicating depature from
    The UCT
    Added revised curriculum vitae

--- teaching/2016/csc1011h/Moss/tutorial1/raw/Ndou, Given(ndxgiv001)/Appointment.py
+++
@@ -1,9 +0,0 @@
-class Appointment:
-    def __init__(self,doc_id_number=None,pat_idnumber=None,timestamp=None,memo=None):
-        self.pat_idnumber=pat_idnumber
-        self.doc_id_number=doc_id_number
-        self.timestamp=timestamp
-        self.memo=memo
-    def __str__(self):
-        return str(self.doc_id_number)+","+str(self.pat_idnumber)+","+str(self.timestamp)+","+self.memo
-    
--- teaching/2016/csc1011h/Moss/tutorial3/raw/Ndlovu, Mxoleleni(ndlmxo003)/Assgn_3.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-stock = sqlite3.connect("All_Stock.db")
-stock.execute("insert into Stock values (?,?,?,?,?,?)",(1,"washing soap","for laundry",18,20,15))
-
-stock.commit()
-
-print(stock)
